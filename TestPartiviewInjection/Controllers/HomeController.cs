eee34dbc714f056cd951f7e38a3e155dd879e868
sanjayforgithub <sanjay.soniji@gmail.com>
2018-04-29 19:50:58 +0530

    first 2

---
+++ TestPartiviewInjection/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestPartiviewInjection.Models;
+
+namespace TestPartiviewInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return PartialView("_contactus");
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestPartiviewInjection/Services/ViewRenderServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestPartiviewInjection.Services
+{
+    public class ViewRenderServices : IViewRenderService
+    {
+        public string RenderHTML()
+        {
+            return "<div><h1>Hello World##PARTIALVIEW##</h1></div>";
+        }
+    }
+}
